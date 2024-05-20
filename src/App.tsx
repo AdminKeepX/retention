704a52d76799dbf6e5c47f0b40ad8abc5d17f424
PavelVaskou <3635019@gmail.com>
2024-05-20 22:06:33 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,20 @@ import { CHAIN } from "@tonconnect/protocol";
 import "@twa-dev/sdk";
 import TabBar from "./Tabbar";
 
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Tab1Content from "./Tab1Content";
+import Tab2Content from "./Tab2Content";
+import Tab3Content from "./Tab3Content";
+
+
 const StyledApp = styled.div`
   background-color: #ffffff;
   color: ${"var(--black-color)"};
@@ -23,29 +37,71 @@ const AppContainer = styled.div`
   margin: 0 auto;
 `;
 
+// function App() {
+//   const { network } = useTonConnect();
+
+//   return (
+//     <StyledApp>
+//       <AppContainer>
+//         <FlexBoxCol>
+//           <FlexBoxRow>
+//             <TonConnectButton />
+//             <Button>
+//               {network
+//                 ? network === CHAIN.MAINNET
+//                   ? "mainnet"
+//                   : "testnet"
+//                 : "N/A"}
+//             </Button>
+            
+//           </FlexBoxRow>
+          
+//           <TabBar />
+//         </FlexBoxCol>
+//       </AppContainer>
+//     </StyledApp>
+//   );
+// }
+
+
+
 function App() {
-  const { network } = useTonConnect();
+  const [value, setValue] = React.useState(0);
+
+  let content;
+  switch (value) {
+    case 0:
+      content = <Tab1Content />;
+      break;
+    case 1:
+      content = <Tab2Content />;
+      break;
+    case 2:
+      content = <Tab3Content />;
+      break;
+    default:
+      content = <Tab1Content />;
+  }
 
   return (
-    <StyledApp>
-      <AppContainer>
-        <FlexBoxCol>
-          <FlexBoxRow>
-            <TonConnectButton />
-            <Button>
-              {network
-                ? network === CHAIN.MAINNET
-                  ? "mainnet"
-                  : "testnet"
-                : "N/A"}
-            </Button>
-            
-          </FlexBoxRow>
-          
-          <TabBar />
-        </FlexBoxCol>
-      </AppContainer>
-    </StyledApp>
+    <Box sx={{ width: '100%', height: '100vh', display: 'flex', flexDirection: 'column' }}>
+    <Box sx={{ flexGrow: 1, padding: 2 }}>
+      {content}
+    </Box>
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Paper>
+  </Box>
   );
 }
 
